7a767bfa9ff25937ec97ab63742865c1ef6261d3
Ed <edd_hunt@yahoo.co.uk>
2020-06-16 14:15:45 +0100

    Update basicAuth system
    BasicAuth class which creates .basicAuthHeader attribute.
    .basicAuthHeader is a dictionary containing attribute "Authorization": "ascii-decode of b64 encoded api key"

--- web_connect/RESTConnect/basicAuth.py
+++ web_connect/RESTConnect/basicAuth.py
@@ -2,12 +2,12 @@ from base64 import b64encode
 
 class BasicAuth:
 
-    def __init__(appKey):
-        self.appKey = appKey
+    def __init__(self,appKey):
+        self.appKey = bytes(appKey,"utf-8")
         self.buildHeader()
 
     def buildHeader(self):
-        userString = self.appKey+":"
+        userString = self.appKey+b":"
         encodedUserString = b64encode(userString)
         decodedUserString = encodedUserString.decode("ascii")
         self.basicAuthHeader = {"Authorization": "Basic " + decodedUserString}
